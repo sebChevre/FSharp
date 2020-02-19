630009d02225a0e0ea169436cc38a0f8ea8b4574
Sébastien Chèvre <seb.chevre@gmail.com>
2020-02-19 16:02:03 +0100

    prompt change

--- Console/Application/Prompt/RecursiveConsolePrompt.fs
+++ Console/Application/Prompt/RecursiveConsolePrompt.fs
@@ -5,6 +5,8 @@ open System
 
 open Application.Prompt.CommonPrompt
 open Application.Repository.ElevesRepository
+open Application.Repository.BulletinDeNotesRepository
+open Application.Domaine.BulletinDeNotes
 
 module ConsolePrompt =
 
@@ -13,28 +15,38 @@ module ConsolePrompt =
             |true, numero -> Some numero
             |false, _ -> None
 
-    let dealInputPrompt = fun (inputChoice:string, cb)->
+    let printEleveChoiceResult optEleve = 
+        match optEleve with
+        |Some eleve -> printfn "%A\n" eleve
+        |None -> printfn "no eleve found with this numero\n"  
+
+    let prettyPrintEleve eleve =
+        printfn "%A" eleve
+
+    let prettyPrintBulletin (bulletin:BulletinEleve) =
+        printfn "%A" bulletin
+
+    let dealInputPrompt = fun (inputChoice:string, promptLoop)->
         
         match inputChoice with
         |"q" -> 
             printfn "Bye!!!"
         |"l" ->
             let eleves = getAllEleves
-            getAllEleves |> printfn "%A"
-            //printfn "All eleves...to be implemented\n"
-            cb()
+            eleves |> List.iter prettyPrintEleve
+            promptLoop()
+        |"b"  ->
+            let bulletins = getAllBulletins
+            bulletins |> List.iter prettyPrintBulletin
         | numero ->
             match choixIsInt numero with
             |Some digit -> 
-                let eleve = findElevesByNumero (digit)
-                
-                match eleve with
-                |Some eleve -> printfn "%A\n" eleve
-                |None -> printfn "no eleve found with this numero\n"
-                cb()        
+                let eleve = findElevesByNumero digit
+                printEleveChoiceResult eleve
+                promptLoop()        
             |None -> 
                 printfn "Only digit to search eleve\n"
-                cb()
+                promptLoop()
 
 
     let getInput () =
